30198e2bc0e41400706ef8475de2b08b2164a7a6
Hagen M?bius <hagen.moebius@googlemail.com>
2010-04-20 21:14:54 +0200

    Removed unnecessary Format field and vlaues in Device. Ther were not used at all.

--- source/Device.java
+++ source/Device.java
@@ -1,36 +1,19 @@
 class Device
 {
-	public static final int DONTCARE = 1;
-	public static final int PROFESSIONAL = 2;
-	public static final int CONSUMER = 3;
-	public static final int ADAT = 4;
-	
-	private String m_Name;
-	private int m_Format;
+	private String _name;
 	
 	public Device()
 	{
-		m_Name = new String();
-		m_Format = DONTCARE;
+		_name = new String();
 	}
 	
-	public void setName(String Name)
+	public void setName(String name)
 	{
-		m_Name = Name;
+		_name = name;
 	}
 	
 	public String getName()
 	{
-		return m_Name;
-	}
-	
-	public void setName(int Format)
-	{
-		m_Format = Format;
-	}
-	
-	public int getFormat()
-	{
-		return m_Format;
+		return _name;
 	}
 }
